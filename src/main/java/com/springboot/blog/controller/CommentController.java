0bd1b87e8d7a505acb4637d8c263b20a1ff6ff2d
Adilet K <adiletkdev@gmail.com>
2023-06-07 18:18:27 +0600

    add: get comment by post id

--- src/main/java/com/springboot/blog/controller/CommentController.java
+++ src/main/java/com/springboot/blog/controller/CommentController.java
@@ -19,7 +19,7 @@ public class CommentController {
 
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity<CommentDto> createComment(
-            @PathVariable(name = "postId") long postId,
+            @PathVariable(value = "postId") long postId,
             @RequestBody CommentDto commentDto
     ) {
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
@@ -28,5 +28,14 @@ public class CommentController {
     @GetMapping("/posts/{postId}/comments")
     public List<CommentDto> getCommentsByPostId(@PathVariable(name = "postId") long postId) {
         return commentService.getCommentsByPostId(postId);
+    }
+
+    @GetMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> getCommentById(
+            @PathVariable(name = "postId") long postId,
+            @PathVariable(name = "id") long commentId
+    ) {
+        CommentDto commentDto = commentService.getCommentById(postId, commentId);
+        return new ResponseEntity<>(commentDto, HttpStatus.OK);
     }
 }
